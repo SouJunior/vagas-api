9296b3bf4f69735dbb0430253791bfd0b1603d19
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2022-09-06 01:00:31 -0300

    feat: add entity job

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JobsModule } from './modules/jobs/jobs.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import 'dotenv/config';
 
 @Module({
-  imports: [JobsModule],
+  imports: [
+    JobsModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      logging: true,
+      migrationsRun: true,
+      entities: ['dist/entities/*.entity.js'],
+      migrations: ['dist/migrations/*.js', 'dist/migrations/seeds/*.js'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    {
+      cors: true,
+    },
+  );
+  await app.listen(process.env.PORT);
 }
 bootstrap();
