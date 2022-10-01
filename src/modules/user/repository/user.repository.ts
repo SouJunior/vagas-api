3520c3a2c3a5046f375ff514342d12521311395d
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2022-10-01 17:00:44 -0300

    feat: implements query params to get paginations values

--- src/modules/user/repository/user.repository.ts
+++ src/modules/user/repository/user.repository.ts
@@ -2,6 +2,7 @@ import { UserEntity } from '../../../database/entities/users.entity';
 import { EntityRepository, Repository } from 'typeorm';
 import { CreateUserDto } from '../dtos/create-user.dto';
 import { UpdateUserDto } from '../dtos/update-user.dto';
+import { PageOptionsDto, PageDto, PageMetaDto } from 'src/shared/pagination';
 
 @EntityRepository(UserEntity)
 export class UserRepository extends Repository<UserEntity> {
@@ -9,10 +10,23 @@ export class UserRepository extends Repository<UserEntity> {
     return this.save(data);
   }
 
-  async getAllUsers(): Promise<UserEntity[]> {
-    return this.find({
-      select: ['id', 'name', 'email', 'created_at'],
-    });
+  async getAllUsers(
+    pageOptionsDto: PageOptionsDto,
+  ): Promise<PageDto<UserEntity>> {
+    const queryBuilder = this.createQueryBuilder('users');
+
+    queryBuilder
+      .orderBy(`users.${pageOptionsDto.orderByColumn}`, pageOptionsDto.order)
+      .select(['users.id', 'users.name', 'users.email', 'users.created_at'])
+      .skip(pageOptionsDto.skip)
+      .take(pageOptionsDto.take);
+
+    const itemCount = await queryBuilder.getCount();
+    const { entities } = await queryBuilder.getRawAndEntities();
+
+    const pageMetaDto = new PageMetaDto({ itemCount, pageOptionsDto });
+
+    return new PageDto(entities, pageMetaDto);
   }
 
   async searchUserByName(name: string): Promise<UserEntity[]> {
