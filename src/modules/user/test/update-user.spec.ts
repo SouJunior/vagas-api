cc4593ff841bf0fc5197c7487a11471f5e4d60ef
Daniel Vinhas <vinhas.daniel@gmail.com>
2022-12-01 08:39:53 -0300

    test[UpdateUser]: ensure update User updates

--- src/modules/user/test/update-user.spec.ts
+++ src/modules/user/test/update-user.spec.ts
@@ -1,7 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CreateUserDto } from '../dtos/create-user.dto';
+import { UserEntity } from 'src/database/entities/users.entity';
+import { UpdateUserDto } from '../dtos/update-user.dto';
 import { UserRepository } from '../repository/user.repository';
 import { UpdateUserService } from '../services/update-user.service';
+
+enum UserRole {
+  ADMIN = 'ADMIN',
+  USER = 'USER',
+}
+
+const testUpdatedData: UpdateUserDto = {
+  email: 'any_email@mail.com',
+  name: 'any_name',
+  password: 'any_password',
+  type: UserRole.USER,
+};
+const testUser: UserEntity = {
+  id: 1,
+  email: 'any_email@mail.com',
+  name: 'any_name',
+  password: 'any_password',
+  type: UserRole.USER,
+  created_at: new Date(),
+  updated_at: new Date(),
+  updateTimestamp() {
+    this.updated_at = new Date();
+  },
+};
 
 class UserRepositoryMock {
   createUser = jest.fn();
@@ -16,25 +41,6 @@ class UserRepositoryMock {
 describe('UpdateUserService', () => {
   let service: UpdateUserService;
   let userRepository: UserRepositoryMock;
-
-  enum UserRole {
-    ADMIN = 'ADMIN',
-    USER = 'USER',
-  }
-
-  const user1: CreateUserDto = {
-    email: 'jhonas@brother.com',
-    name: 'Jhonas',
-    password: 'zarabara',
-    type: UserRole.USER,
-  };
-
-  const user2: CreateUserDto = {
-    email: 'castro@brothers.com',
-    name: 'Castro',
-    password: 'zarabara',
-    type: UserRole.USER,
-  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -53,19 +59,14 @@ describe('UpdateUserService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(userRepository).toBeDefined();
   });
 
-  test('should not be able to update when user not exist.', () => {
-    it('should', async () => {
-      const newUser = await userRepository.createUser(user1);
-      const data = {
-        name: 'Alex',
-      };
-      // const result = await service.execute(id, data);
+  it('should be able to update an user with correct data.', async () => {
+    userRepository.findOneById = jest.fn().mockResolvedValueOnce(testUser);
+    userRepository.updateUser = jest.fn().mockResolvedValueOnce(testUser);
 
-      expect(await service.execute(newUser.id, data)).toContain({
-        name: 'alex',
-      });
-    });
+    const result = await service.execute(testUser.id, testUpdatedData);
+    expect(result).toEqual(testUser);
   });
 });
