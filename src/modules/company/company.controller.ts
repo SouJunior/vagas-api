3a94b60d24f5ee6263ae72d7b8481b895ce8895c
YtalloCesar2022 <100654478+YtalloCesar2022@users.noreply.github.com>
2023-06-17 15:08:44 -0300

    feat: insert api properity in update company for swagger

--- src/modules/company/company.controller.ts
+++ src/modules/company/company.controller.ts
@@ -14,8 +14,15 @@ import {
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiBearerAuth, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { Response } from 'express';
 import { CompaniesEntity } from 'src/database/entities/companies.entity';
 import { BadRequestSwagger } from '../../shared/Swagger/bad-request.swagger';
@@ -37,7 +44,6 @@ import {
 import { ActivateCompanyService } from './services/activate-company.service';
 import { RecoveryCompanyPasswordByEmail } from './services/recovery-password-by-email.service';
 import { UpdatePasswordByEmailService } from './services/update-password-by-email.service';
-import { AuthGuard } from '@nestjs/passport';
 
 @ApiTags('Company')
 @Controller('company')
