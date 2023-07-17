875e8c7af876d0c36c1a7ccbb6dec3ce1096a7b8
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2023-07-17 19:33:28 -0300

    feat: add new fields in user update

--- src/modules/user/dtos/update-user.dto.ts
+++ src/modules/user/dtos/update-user.dto.ts
@@ -1,4 +1,29 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty({ required: true, example: '11111111111' })
+  @Transform(({ value }) => value.replace(/\D/g, ''))
+  @IsNotEmpty()
+  @IsString()
+  mainPhone: string;
+
+  @ApiProperty({ required: false, example: '11111111111' })
+  @Transform(({ value }) => value.replace(/\D/g, ''))
+  @IsOptional()
+  @IsString()
+  phone: string;
+
+  @ApiProperty({ required: true, example: 'Rio Branco' })
+  @IsNotEmpty()
+  @IsString()
+  city: string;
+
+  @ApiProperty({ required: true, example: 'Acre' })
+  @IsNotEmpty()
+  @IsString()
+  state: string;
+}
