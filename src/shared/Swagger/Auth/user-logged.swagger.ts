44ae6dc073341d60f6702d01c33111b3926b7d32
Patrick Otero <patrick.rocha.otero@gmail.com>
2023-09-07 10:41:33 -0300

    Cleaning - Auth controller

---
+++ src/shared/Swagger/Auth/user-logged.swagger.ts
@@ -0,0 +1,22 @@
+import { HttpStatus, applyDecorators } from "@nestjs/common";
+import { ApiOperation, ApiResponse } from "@nestjs/swagger";
+import { LoggerUserType } from "src/modules/auth/types/logged-user.types";
+import { UnauthorizedSwagger } from "../unauthorized.swagger";
+
+export function UserLoggedSwagger(){
+    return applyDecorators(
+        ApiResponse({
+            status: HttpStatus.OK,
+            description: 'Exemplo do retorno de sucesso da rota',
+            type: LoggerUserType,
+          }),
+          ApiResponse({
+            status: HttpStatus.UNAUTHORIZED,
+            description: 'Modelo de erro',
+            type: UnauthorizedSwagger,
+          }),
+          ApiOperation({
+            summary: 'Retorna usuário logado',
+          })
+    )
+}
