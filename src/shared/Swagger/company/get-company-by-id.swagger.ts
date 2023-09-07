0caa13afb9a39229b3434ee309db91a1df25fc6b
Patrick Otero <patrick.rocha.otero@gmail.com>
2023-09-07 10:18:13 -0300

    All the codes referred to swagger routes were moved from the company controller to separate fil

---
+++ src/shared/Swagger/company/get-company-by-id.swagger.ts
@@ -0,0 +1,26 @@
+import { HttpStatus, applyDecorators } from "@nestjs/common";
+import { ApiOperation, ApiParam, ApiResponse } from "@nestjs/swagger";
+import { UnauthorizedSwagger } from "../unauthorized.swagger";
+import { BadRequestSwagger } from "../bad-request.swagger";
+
+export function GetCompanyByIdSwagger() {
+    return applyDecorators(
+        ApiParam({
+            name: 'id',
+            type: 'string',
+          }),
+          ApiResponse({
+            status: HttpStatus.UNAUTHORIZED,
+            description: 'Modelo de erro',
+            type: UnauthorizedSwagger,
+          }),
+          ApiResponse({
+            status: HttpStatus.BAD_REQUEST,
+            description: 'Modelo de erro',
+            type: BadRequestSwagger,
+          }),
+          ApiOperation({
+            summary: 'Buscar uma empresa por id.',
+          })
+    )
+}
---
+++ src/shared/Swagger/company/update-password.swagger.ts
@@ -0,0 +1,28 @@
+import { HttpStatus, applyDecorators } from "@nestjs/common";
+import { CreatePasswordHashDto } from "src/modules/company/dtos/update-my-password.dto";
+import { UnauthorizedSwagger } from "../unauthorized.swagger";
+import { ApiOperation, ApiResponse } from "@nestjs/swagger";
+import { BadRequestSwagger } from "../bad-request.swagger";
+
+export function UpdatePasswordSwagger() {
+    return applyDecorators(
+        ApiResponse({
+            status: HttpStatus.OK,
+            description: 'Exemplo do retorno de sucesso da rota',
+            type: CreatePasswordHashDto,
+          }),
+          ApiResponse({
+            status: HttpStatus.UNAUTHORIZED,
+            description: 'Modelo de erro',
+            type: UnauthorizedSwagger,
+          }),
+          ApiResponse({
+            status: HttpStatus.BAD_REQUEST,
+            description: 'Modelo de erro',
+            type: BadRequestSwagger,
+          }),
+          ApiOperation({
+            summary: 'Company update password without recovery e-mail.',
+          })
+    )
+}
