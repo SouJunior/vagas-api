0f5dc96dbdb25586b5be99169e03c9cf57c39575
YtalloCesar2022 <ytallobmw@gmail.com>
2023-10-22 19:57:16 -0300

    refactor company

--- src/modules/company/dtos/create-company.dto.ts
+++ src/modules/company/dtos/create-company.dto.ts
@@ -10,55 +10,59 @@ import {
 } from 'class-validator';
 
 export class CreateCompanyDto {
-  @IsString()
-  @IsNotEmpty()
-  @MaxLength(30)
   @ApiProperty({
-    description: 'Nome da empresa',
+    description: 'Nome da empresa (até 30 caracteres)',
     example: 'Pipomills',
   })
+  @IsString({ message: 'O campo companyName deve ser uma string' })
+  @IsNotEmpty({ message: 'O campo companyName não pode estar vazio' })
+  @MaxLength(30)
   companyName: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @IsEmail()
   @ApiProperty({
     description: 'Email da empresa',
     example: 'pipomills@pipomills.com',
   })
+  @IsString({ message: 'O campo email deve ser uma string' })
+  @IsNotEmpty({ message: 'O campo email não pode estar vazio' })
+  @IsEmail()
   email: string;
 
-  @IsNotEmpty()
-  @IsString()
-  @MaxLength(14)
-  @MinLength(14)
   @ApiProperty({
-    description: 'CNPJ',
+    description: 'CNPJ (formato: xx.xxx.xxx/xxxx-xx)',
     example: '67.979.311/0001-15',
   })
+  @IsString({ message: 'O campo cnpj deve ser uma string' })
+  @IsNotEmpty({ message: 'O campo cnpj não pode estar vazio' })
+  @MaxLength(14)
+  @MinLength(14)
   cnpj: string;
 
-  @IsNotEmpty()
-  @IsString()
-  @Length(8, 20)
-  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message: 'Senha muito fraca',
-  })
   @ApiProperty({
-    description: 'Senha de Login',
+    description:
+      'Senha de Login (8 a 20 caracteres, deve conter letras maiúsculas, minúsculas, números e caracteres especiais)',
     example: 'Abcd@1234',
   })
+  @IsString({ message: 'O campo password deve ser uma string' })
+  @IsNotEmpty({ message: 'O campo password não pode estar vazio' })
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'A senha deve conter letras maiúsculas, minúsculas, números e caracteres especiais',
+  })
+  @Length(8, 20)
   password: string;
 
-  @IsNotEmpty()
-  @IsString()
-  @Length(8, 20)
-  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message: 'Senha muito fraca',
-  })
   @ApiProperty({
-    description: 'Confirmação de Senha de Login',
+    description:
+      'Confirmação de Senha de Login (8 a 20 caracteres, deve ser idêntica à senha)',
     example: 'Abcd@1234',
   })
+  @IsString({ message: 'O campo passwordConfirmation deve ser uma string' })
+  @IsNotEmpty({ message: 'O campo passwordConfirmation não pode estar vazio' })
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'A confirmação de senha deve conter letras maiúsculas, minúsculas, números e caracteres especiais',
+  })
+  @Length(8, 20)
   passwordConfirmation: string;
 }
--- src/modules/company/dtos/update-my-password.dto.ts
+++ src/modules/company/dtos/update-my-password.dto.ts
@@ -3,66 +3,71 @@ import { IsNotEmpty, IsString, Length, Matches } from 'class-validator';
 import { Match } from '../dtos/decorators/match.decorator';
 
 export class UpdateMyPasswordDto {
-  @IsString()
-  @IsNotEmpty()
+  @ApiProperty({
+    description: 'Senha atual',
+    example: 'Abcd@1234',
+  })
+  @IsString({ message: 'O campo oldPassword deve ser uma string' })
+  @IsNotEmpty({ message: "O campo 'oldPassword' não pode ficar vazio" })
   oldPassword: string;
 
-  @IsString()
+  @ApiProperty({
+    description: 'Nova senha de login',
+    example: 'Abcd@1234',
+  })
+  @IsString({ message: 'O campo password deve ser uma string' })
   @IsNotEmpty({ message: "O campo 'password' não pode ficar vazio" })
   @Length(8, 50)
   @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
     message:
       'A senha precisa ter no mínimo 8 caracteres, máximo de 50, uma letra maiúscula, um número e um símbolo.',
   })
-  @ApiProperty({
-    description: 'Senha de Login',
-    example: 'Abcd@1234',
-  })
   password: string;
 
-  @IsString()
-  @IsNotEmpty({ message: "O campo 'confirmPassword' não pode ficar vazio" })
+  @ApiProperty({
+    description: 'Confirmação da nova senha de login',
+    example: 'Abcd@1234',
+  })
+  @IsString({ message: 'O campo confirmNewPassword deve ser uma string' })
+  @IsNotEmpty({ message: "O campo 'confirmNewPassword' não pode ficar vazio" })
   @Length(8, 50)
   @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
     message:
       'A senha precisa ter no mínimo 8 caracteres, máximo de 50, uma letra maiúscula, um número e um símbolo.',
-  })
-  @ApiProperty({
-    description: 'Senha de Login',
-    example: 'Abcd@1234',
   })
   confirmNewPassword: string;
 }
 
 export class CreatePasswordHashDto {
+  @ApiProperty({
+    description:
+      'Senha de login (8 a 50 caracteres, com pelo menos uma letra maiúscula, um número e um caractere especial)',
+    example: 'Abcd@1234',
+  })
+  @IsString({ message: 'O campo password deve ser uma string' })
   @IsNotEmpty({ message: "O campo 'password' não pode ficar vazio" })
-  @IsString()
   @Length(8, 50)
   @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
     message:
       'A senha precisa ter no mínimo 8 caracteres, máximo de 50, uma letra maiúscula, um número e um símbolo.',
   })
-  @ApiProperty({
-    description: 'Senha de Login',
-    example: 'Abcd@1234',
-  })
   password: string;
 
-  @IsNotEmpty({ message: "O campo 'confirmPassword' não pode ficar vazio" })
-  @IsString()
   @ApiProperty({
-    description: 'Confirmação de senha',
-    example: 'Abcd@123',
+    description: 'Confirmação de senha (deve ser idêntica à senha)',
+    example: 'Abcd@1234',
   })
+  @IsString({ message: 'O campo confirmPassword deve ser uma string' })
+  @IsNotEmpty({ message: "O campo 'confirmPassword' não pode ficar vazio" })
   @Match('password', {
     message: 'Os campos de senha precisam ser idênticos.',
   })
   confirmPassword: string;
 
-  @IsString()
   @ApiProperty({
-    description: 'token de recuperação de senha.',
-    example: 'algo uuid bem rândomico',
+    description: 'Token de recuperação de senha (opcional)',
+    example: 'algum UUID aleatório',
   })
+  @IsString({ message: 'O campo recoverPasswordToken deve ser uma string' })
   recoverPasswordToken?: string;
 }
