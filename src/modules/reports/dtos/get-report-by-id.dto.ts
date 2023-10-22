bedc82b42bee77b9a0649b75fd58ac3a1528c223
YtalloCesar2022 <ytallobmw@gmail.com>
2023-10-22 20:14:28 -0300

    refactor: update curriculum and reports dto

--- src/modules/reports/dtos/get-report-by-id.dto.ts
+++ src/modules/reports/dtos/get-report-by-id.dto.ts
@@ -2,11 +2,11 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class ReportIdDto {
-  @IsString()
-  @IsNotEmpty()
   @ApiProperty({
-    description: 'ID do report',
+    description: 'ID do relatório',
     example: 'be02e7b0-238a-44c2-b9db-ccb339d63fc9',
   })
+  @IsString({ message: 'O campo id deve ser uma string' })
+  @IsNotEmpty({ message: 'O campo id não pode estar vazio' })
   id: string;
 }
