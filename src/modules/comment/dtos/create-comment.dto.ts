ce451ec6e6824ac9dcdd63fb6715825591a14904
YtalloCesar2022 <ytallobmw@gmail.com>
2023-10-22 19:06:12 -0300

    refactor: comments and company dto

--- src/modules/comment/dtos/create-comment.dto.ts
+++ src/modules/comment/dtos/create-comment.dto.ts
@@ -2,28 +2,28 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class CreateCommentDto {
-  @IsString()
-  @IsNotEmpty()
-  @MaxLength(500)
   @ApiProperty({
-    description: 'Comentário',
+    description: 'Comentário (até 500 caracteres)',
     example: 'Boa!',
   })
+  @IsString({ message: 'O campo comment deve ser uma string' })
+  @IsNotEmpty({ message: 'O campo comment não pode estar vazio' })
+  @MaxLength(500)
   comment: string;
 
-  @IsString()
-  @IsNotEmpty()
   @ApiProperty({
     description: 'ID do trabalho',
     example: 'be02e7b0-238a-44c2-b9db-ccb339d63fc9',
   })
+  @IsString({ message: 'O campo job_id deve ser uma string' })
+  @IsNotEmpty({ message: 'O campo job_id não pode estar vazio' })
   job_id: string;
 
-  @IsString()
-  @IsNotEmpty()
   @ApiProperty({
-    description: 'ID do produto',
+    description: 'ID do usuário',
     example: 'be02e7b0-238a-44c2-b9db-ccb339d63fc9',
   })
+  @IsString({ message: 'O campo user_id deve ser uma string' })
+  @IsNotEmpty({ message: 'O campo user_id não pode estar vazio' })
   user_id: string;
 }
