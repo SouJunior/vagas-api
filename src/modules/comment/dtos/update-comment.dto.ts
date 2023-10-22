82bc29886092a5fd1aba7cc477bb98768549dfe8
YtalloCesar2022 <ytallobmw@gmail.com>
2023-10-22 16:14:53 -0300

    refactor: update dto

--- src/modules/comment/dtos/update-comment.dto.ts
+++ src/modules/comment/dtos/update-comment.dto.ts
@@ -1,8 +1,8 @@
 import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class UpdateCommentDto {
-  @IsString()
-  @IsNotEmpty()
+  @IsString({ message: 'O campo comment deve ser uma string' })
+  @IsNotEmpty({ message: 'O campo comment não pode estar vazio' })
   @MaxLength(500)
   comment: string;
 }
