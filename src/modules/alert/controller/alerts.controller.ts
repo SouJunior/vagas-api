694f7fc7548c27d1a5e65734aa86bb8cfd91989a
Guilherme Sanches <guitdsanches@gmail.com>
2024-10-04 16:32:00 -0300

    :sparkles: feat(alerts): add swagger annotations

--- src/modules/alert/controller/alerts.controller.ts
+++ src/modules/alert/controller/alerts.controller.ts
@@ -3,6 +3,7 @@ import { AlertsService } from '../service/alerts.service';
 import { CreateAlertDto } from '../dtos/create-alert.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { CreateAlertSwagger } from '../../../shared/Swagger/decorators/alerts/create-alert.swagger';
 
 @ApiTags('Alerts')
 @Controller('alerts')
@@ -12,6 +13,7 @@ export class AlertsController {
 
   @Post()
   @ApiBearerAuth()
+  @CreateAlertSwagger()
   async createAlert(@Body() createAlertDto: CreateAlertDto) {
     return await this.alertsService.createAlert(createAlertDto);
   }
