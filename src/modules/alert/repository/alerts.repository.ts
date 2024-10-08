f2d2d2fbe6ba0ff04003931e3257d37a04067608
Letícia Zalasik <leticiazalasik@gmail.com>
2024-10-08 20:30:05 -0300

    :sparkles: Feat Atualização do repositório local

---
+++ src/modules/alert/repository/alerts.repository.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AlertEntity } from '../../../database/entities/alert.entity';
+
+@Injectable()
+export class AlertsRepository {
+  constructor(
+    @InjectRepository(AlertEntity)
+    private alertsRepository: Repository<AlertEntity>,
+  ) {}
+
+  async createAlert(data: Partial<AlertEntity>): Promise<AlertEntity> {
+    return this.alertsRepository.save(data);
+  }
+
+  async findAlertsByUserId(userId: string): Promise<AlertEntity[]> {
+    return this.alertsRepository.find({ where: { user: { id: userId } } });
+  }
+
+  
+}
