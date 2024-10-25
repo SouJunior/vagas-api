82c657743dfab57fd53c3208b8e20fdb48c065c4
jlazaro <j.lazaro.junior@gmail.com>
2024-10-25 13:25:01 -0300

    feat (Corrigido: Cadidacy.entity.ts.)

--- src/database/entities/candidacy.entity.ts
+++ src/database/entities/candidacy.entity.ts
@@ -4,37 +4,40 @@ import {
   PrimaryGeneratedColumn,
   ManyToOne,
   JoinColumn,
-} from 'typeorm';
-import { UsersEntity } from './users.entity'; 
-import { JobsEntity } from './jobs.entity'; 
-import { CandidacyStatus } from './candidancy-status.enum';
-
-@Entity('tb_candidacies') 
-export class CandidacyEntity {
+  } from 'typeorm';
+  import { UsersEntity } from './users.entity';
+  import { JobsEntity } from './jobs.entity';
+  import { CandidacyStatus } from './candidancy-status.enum';
+  
+  @Entity('tb_candidacies', { indexes: [{ columns: ['userId'] }, { columns: ['jobId'] }] })
+  export class CandidacyEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
-
+  
   @Column('uuid')
   jobId: string;
-
+  
   @Column('uuid')
-  userId: string; 
-
+  userId: string;
+  
   @Column({ type: 'enum', enum: CandidacyStatus })
   status: CandidacyStatus;
-
+  
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   dateCandidacy: Date;
-
-  @Column({ type: 'timestamp', nullable: true })
+  
+  @Column({
+  type: 'timestamp',
+  nullable: true,
+  onUpdate: () => 'CURRENT_TIMESTAMP'
+  })
   dateClosing: Date;
-
+  
   @ManyToOne(() => UsersEntity)
   @JoinColumn({ name: 'userId' })
-  user: UsersEntity; 
-
+  user: UsersEntity;
+  
   @ManyToOne(() => JobsEntity)
   @JoinColumn({ name: 'jobId' })
   job: JobsEntity;
-}
-
+  }
