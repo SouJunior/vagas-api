e382ed63a2480992b8d2926cf4f10c1af518f757
joselazarojunior <j.lazaro.junior@gmail.com>
2024-11-22 21:55:06 -0300

    feat(endpoint): Criar endpoint para atualizar candidatura

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,18 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Req,
+  Res,
+  Patch,
+  Param,
+  Body,
+  NotFoundException,
+  BadRequestException,
+  HttpCode,
+} from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 import { SwaggerHealthCheck } from './shared/Swagger/decorators/app/health-check.swagger.decorator';
-
 import { Request, Response } from 'express';
 
 @ApiTags('Status')
@@ -24,8 +34,34 @@ export class AppController {
   @ApiOperation({
     summary: 'Retorna status dos serviços de email e banco de dados',
   })
-  async getHealthCheck(@Res() res: Response){
-    const {status, data} =  await this.appService.getHealthCheck();
+  async getHealthCheck(@Res() res: Response) {
+    const { status, data } = await this.appService.getHealthCheck();
     return res.status(status).send(data);
+  }
+
+  @Patch(':applicationId')
+  @HttpCode(200)
+  async updateStatus(
+    @Param('userId') userId: string,
+    @Param('applicationId') applicationId: string,
+    @Body('status') status: string,
+  ) {
+    if (!status) {
+      throw new BadRequestException('Novo status é obrigatório');
+    }
+
+    const application = await this.appService.updateStatus(
+      userId,
+      applicationId,
+      status,
+    );
+
+    if (!application) {
+      throw new NotFoundException(
+        'Candidatura não encontrada ou não pertence ao usuário',
+      );
+    }
+
+    return { message: 'Status atualizado com sucesso', application };
   }
 }
