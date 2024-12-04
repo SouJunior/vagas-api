56d95c187e273effdf5418b947dbfc572414dd27
Letícia Zalasik <leticiazalasik@gmail.com>
2024-12-03 21:16:46 -0300

    :construction: Fix: (applications.entity) Indíces na chave estrangeira para otimizar consultas, decorators para validar dados da entidade e nomes mais descritivos para created e update at.

--- src/database/entities/applications.entity.ts
+++ src/database/entities/applications.entity.ts
@@ -2,6 +2,7 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  Index,
   JoinColumn,
   ManyToOne,
   PrimaryGeneratedColumn,
@@ -10,38 +11,51 @@ import {
 import { CurriculumEntity } from './curriculum.entity';
 import { JobsEntity } from './jobs.entity';
 import { UsersEntity } from './users.entity';
+import { IsUUID, IsNotEmpty } from 'class-validator';
+
 
 @Entity('tb_applications')
 export class ApplicationEntity {
   @PrimaryGeneratedColumn('uuid')
+  @IsUUID()
   id: string;
 
   @ManyToOne(() => JobsEntity, { onDelete: "CASCADE" })
   @JoinColumn({ name: 'job_id' })
+  @Index()
   job: JobsEntity;
 
   @Column()
+  @IsUUID() 
+  @IsNotEmpty()
   job_id: string;
 
   @ManyToOne(() => UsersEntity, { onDelete: "CASCADE" })
   @JoinColumn({ name: 'user_id' })
+  @Index()
   user: UsersEntity;
 
   @Column()
+  @IsUUID() 
+  @IsNotEmpty()
+
   user_id: string;
 
   @ManyToOne(() => CurriculumEntity, { onDelete: "CASCADE" })
   @JoinColumn({ name: 'curriculum_id' })
+  @Index()
   curriculum: CurriculumEntity;
 
   @Column()
+  @IsUUID() 
+  @IsNotEmpty()
   curriculum_id: string;
 
   @CreateDateColumn()
-  created_at: Date;
+  created_date_time: Date;
 
   @UpdateDateColumn({ update: true })
-  updated_at: Date;
+  updated_date_tima: Date;
   status: any;
 
 }
