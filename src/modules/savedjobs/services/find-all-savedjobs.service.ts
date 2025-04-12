25bcbaf18bb3027b747c17293c2c8df82f5173c2
larisarapio <larissa.sarapio@gmail.com>
2025-04-12 15:44:00 -0300

    edit: ajuste nome variavel

--- src/modules/savedjobs/services/find-all-savedjobs.service.ts
+++ src/modules/savedjobs/services/find-all-savedjobs.service.ts
@@ -4,15 +4,14 @@ import { PageDto, PageOptionsDto } from "src/shared/pagination";
 import { SavedJobsRepository } from "../repository/savedjobs.repository";
 import { GetAllSavedJobsDto } from "../dtos/get-all-savedjobs.dto";
 
-
 @Injectable()
 export class FindAllSavedJobsService {
-  constructor(private savedJobRepository: SavedJobsRepository) {}
+  constructor(private readonly savedJobsRepository: SavedJobsRepository) {}
 
   async execute(
     pageOptionsDto: PageOptionsDto,
     filters: GetAllSavedJobsDto,
   ): Promise<PageDto<SavedJobsEntity>> {
-    return this.savedJobRepository.getAllSavedJobs(pageOptionsDto, filters);
+    return this.savedJobsRepository.getAllSavedJobs(pageOptionsDto, filters);
   }
 }
