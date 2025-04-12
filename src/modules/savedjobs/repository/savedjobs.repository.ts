13ff3d54cba5b24be76f4f6e032aa84e7f0f3e4a
larisarapio <larissa.sarapio@gmail.com>
2025-04-12 15:36:31 -0300

    edit: ajuste do retorno do metodo, try catch e nome variavel

--- src/modules/savedjobs/repository/savedjobs.repository.ts
+++ src/modules/savedjobs/repository/savedjobs.repository.ts
@@ -1,8 +1,7 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, InternalServerErrorException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { SavedJobsEntity } from "src/database/entities/savedjobs.entity";
 import { PageDto, PageMetaDto, PageOptionsDto } from "src/shared/pagination";
-import { handleError } from "src/shared/utils/handle-error.util";
 import { Repository } from "typeorm";
 import { GetAllSavedJobsDto } from "../dtos/get-all-savedjobs.dto";
 
@@ -10,33 +9,36 @@ import { GetAllSavedJobsDto } from "../dtos/get-all-savedjobs.dto";
 export class SavedJobsRepository {
   constructor(
     @InjectRepository(SavedJobsEntity)
-    private savedJobsRepository: Repository<SavedJobsEntity>,
+    private readonly savedJobsRepository: Repository<SavedJobsEntity>,
   ) {}
 
   async getAllSavedJobs(
     pageOptionsDto: PageOptionsDto,
     filters: GetAllSavedJobsDto,
   ): Promise<PageDto<SavedJobsEntity>> {
-    const queryBuilder = this.savedJobsRepository
-      .createQueryBuilder('savedJob')
-      .leftJoinAndSelect('savedJob.user', 'user')
-      .orderBy('savedJob.savedAt', pageOptionsDto.order)
-      .skip((pageOptionsDto.page - 1) * pageOptionsDto.take)
-      .take(pageOptionsDto.take);
+    try {
+      const queryBuilder = this.savedJobsRepository
+        .createQueryBuilder('savedJob')
+        .leftJoinAndSelect('savedJob.user', 'user')
+        .orderBy('savedJob.savedAt', pageOptionsDto.order)
+        .skip((pageOptionsDto.page - 1) * pageOptionsDto.take)
+        .take(pageOptionsDto.take);
 
-    if (filters.userId) {
-      queryBuilder.andWhere('user.id = :userId', { userId: filters.userId });
+      if (filters.userId) {
+        queryBuilder.andWhere('user.id = :userId', { userId: filters.userId });
+      }
+
+      if (filters.jobId) {
+        queryBuilder.andWhere('savedJob.jobId = :jobId', { jobId: filters.jobId });
+      }
+
+      const [savedJobs, itemCount] = await queryBuilder.getManyAndCount();
+
+      const pageMetaDto = new PageMetaDto({ itemCount, pageOptionsDto });
+
+      return new PageDto(savedJobs, pageMetaDto);
+    } catch (error) {
+      throw new InternalServerErrorException('Erro ao buscar vagas salvas');
     }
-
-    if (filters.jobId) {
-      queryBuilder.andWhere('savedJob.jobId = :jobId', { jobId: filters.jobId });
-    }
-
-    const itemCount = await queryBuilder.getCount().catch(handleError);
-    const { entities } = await queryBuilder.getRawAndEntities();
-
-    const pageMetaDto = new PageMetaDto({ itemCount, pageOptionsDto });
-
-    return new PageDto(entities, pageMetaDto);
   }
 }
