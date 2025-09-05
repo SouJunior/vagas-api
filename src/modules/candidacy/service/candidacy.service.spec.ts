27f502fd6dc6151396c1c2c76d5031d44a0c61d0
Leidejane <leidejanedarosa.81@gmail.com>
2025-09-05 19:55:30 -0300

    test(modules): modernize candidacy and savedjobs service tests

--- src/modules/candidacy/service/candidacy.service.spec.ts
+++ src/modules/candidacy/service/candidacy.service.spec.ts
@@ -2,27 +2,36 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CandidacyService } from './candidacy.service';
 import { CandidacyRepository } from '../repository/candidacy.repository';
 
-class CandidacyRepositoryMock {
-  create = jest.fn();
-  findOne = jest.fn();
-  update = jest.fn();
-}
+const createCandidacyRepositoryMock = (): jest.Mocked<
+  Partial<CandidacyRepository>
+> => ({
+  createCandidacy: jest.fn(),
+  findAllByUserId: jest.fn(),
+  updateStatus: jest.fn(),
+});
 
 describe('CandidacyService', () => {
   let service: CandidacyService;
+  let candidacyRepository: jest.Mocked<Partial<CandidacyRepository>>;
 
   beforeEach(async () => {
+    candidacyRepository = createCandidacyRepositoryMock();
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         CandidacyService,
         {
           provide: CandidacyRepository,
-          useClass: CandidacyRepositoryMock,
+          useValue: candidacyRepository,
         },
       ],
     }).compile();
 
     service = module.get<CandidacyService>(CandidacyService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
--- src/modules/savedjobs/services/savedjobs.service.spec.ts
+++ src/modules/savedjobs/services/savedjobs.service.spec.ts
@@ -5,37 +5,48 @@ import { SavedJobsEntity } from '../../../database/entities/savedjobs.entity';
 import { UsersEntity } from '../../../database/entities/users.entity';
 import { JobsEntity } from '../../../database/entities/jobs.entity';
 
-class RepositoryMock {
-  create = jest.fn();
-  save = jest.fn();
-  findOne = jest.fn();
-  find = jest.fn();
-  delete = jest.fn();
-}
+const createRepositoryMock = () => ({
+  create: jest.fn(),
+  save: jest.fn(),
+  findOne: jest.fn(),
+  find: jest.fn(),
+  delete: jest.fn(),
+});
 
 describe('SavedjobsService', () => {
   let service: SavedJobsService;
+  let savedJobsRepository: any;
+  let usersRepository: any;
+  let jobsRepository: any;
 
   beforeEach(async () => {
+    savedJobsRepository = createRepositoryMock();
+    usersRepository = createRepositoryMock();
+    jobsRepository = createRepositoryMock();
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         SavedJobsService,
         {
           provide: getRepositoryToken(SavedJobsEntity),
-          useClass: RepositoryMock,
+          useValue: savedJobsRepository,
         },
         {
           provide: getRepositoryToken(UsersEntity),
-          useClass: RepositoryMock,
+          useValue: usersRepository,
         },
         {
           provide: getRepositoryToken(JobsEntity),
-          useClass: RepositoryMock,
+          useValue: jobsRepository,
         },
       ],
     }).compile();
 
     service = module.get<SavedJobsService>(SavedJobsService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
