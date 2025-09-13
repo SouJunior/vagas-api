2444b63926045a69a2eeebc9cb3c962631591b3a
Leidejane <leidejanedarosa.81@gmail.com>
2025-09-13 11:20:31 -0300

    fix: remove duplicate user_id FK in ApplicationEntity and use RelationId

--- src/database/entities/applications.entity.ts
+++ src/database/entities/applications.entity.ts
@@ -2,9 +2,11 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  Index,
   JoinColumn,
   ManyToOne,
   PrimaryGeneratedColumn,
+  RelationId,
   UpdateDateColumn,
 } from 'typeorm';
 import { CurriculumEntity } from './curriculum.entity';
@@ -23,13 +25,13 @@ export class ApplicationEntity {
   @Column()
   job_id: string;
 
-  @ManyToOne(() => UsersEntity, { onDelete: 'CASCADE' })
+  @Index('idx_applications_user_id')
+  @ManyToOne(() => UsersEntity, { onDelete: 'CASCADE', nullable: false })
   @JoinColumn({ name: 'user_id' })
   user: UsersEntity;
 
-  @Column()
+  @RelationId((app: ApplicationEntity) => app.user)
   user_id: string;
-
   @ManyToOne(() => CurriculumEntity, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'curriculum_id' })
   curriculum: CurriculumEntity;
