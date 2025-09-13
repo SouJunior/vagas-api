3542de968efd53c93f1c4848b54364300104a3cb
Leidejane <leidejanedarosa.81@gmail.com>
2025-09-13 07:44:41 -0300

    fix(report): fix report search and update methods

--- src/modules/reports/repository/reports.repository.ts
+++ src/modules/reports/repository/reports.repository.ts
@@ -6,6 +6,7 @@ import { ReportParamsType } from '../types/find-by-params.type';
 import { handleError } from '../../../shared/utils/handle-error.util';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable } from '@nestjs/common';
+import { NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class ReportRepository {
@@ -18,19 +19,18 @@ export class ReportRepository {
     return this.reportsRepository.save(data).catch(handleError);
   }
 
-  async findByParams(params: ReportParamsType): Promise<ReportsEntity> {
+  async findByParams(params: ReportParamsType): Promise<ReportsEntity | null> {
     const { job_id, user_id } = params;
-
     return this.reportsRepository
       .findOneBy({ job_id, user_id })
       .catch(handleError);
   }
 
-  async findAllRepots(): Promise<ReportsEntity[]> {
+  async findAllReports(): Promise<ReportsEntity[]> {
     return this.reportsRepository.find().catch(handleError);
   }
 
-  async findReportById(id: string): Promise<ReportsEntity> {
+  async findReportById(id: string): Promise<ReportsEntity | null> {
     return this.reportsRepository.findOneBy({ id }).catch(handleError);
   }
 
@@ -39,12 +39,12 @@ export class ReportRepository {
       .findOneBy({ id })
       .catch(handleError);
 
-    return this.reportsRepository
-      .save({
-        ...report,
-        ...data,
-      })
-      .catch(handleError);
+    if (!report) {
+      throw new NotFoundException('Relatório não encontrado');
+    }
+
+    this.reportsRepository.merge(report, data);
+    return this.reportsRepository.save(report).catch(handleError);
   }
 
   async deleteReportById(id: string): Promise<object> {
